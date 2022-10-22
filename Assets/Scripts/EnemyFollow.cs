ea978e5a270ef2e4f5b279075402e659a743c269
biboyboy04 <biboyboy04@gmail.com>
2022-10-23 01:53:15 +0800

    Add ghost enemy idle and follow prefabs. Add EnemyFollow.cs
    Add prefabs for ghost idle and follow.
    
    Add EnemyFollow.cs. Following the player when in range and changing the color of the affected enemy.

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -6,23 +6,40 @@ public class EnemyFollow : MonoBehaviour
 {
     public GameObject player;
     public float speed;
+    private SpriteRenderer spriteRend;
+    private Color originColor;
+
 
     private float distance;
     // Start is called before the first frame update
     void Start()
     {
+        
+        spriteRend = GetComponent<SpriteRenderer>();
+    //    originColor = spriteRend.Color;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-      //  direction.Normalize();
-       // float angle = Mathf.Atan2(direction.y, direction.x) + Mathf.Rad2Deg;
+        direction.Normalize();
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        Vector2 playerPosition = new Vector2(player.transform.position.x, player.transform.position.y - 0.5f);
+
+        if (distance < 10)
+        {
+            
+            transform.position = Vector2.MoveTowards(this.transform.position, playerPosition, speed * Time.deltaTime);
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            spriteRend.color = Color.red;
+        }
+        else 
+        {
+             spriteRend.color = Color.white;
+        }
     }
 }
