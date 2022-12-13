b7a0382ee9b80b221c699c82e33544a3fe5cf5b8
biboyboy04 <biboyboy04@gmail.com>
2022-12-14 05:05:34 +0800

    Update HealthBar.cs
    Change HealthBar.cs to  StatusBar.cs to accomodate multiple types of status.

---
+++ Assets/Scripts/StatusBar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Souls playerSoul;
+    [SerializeField] public Slider slider;
+    [SerializeField] public Gradient gradient;
+    public Image fill;
+
+    private void Start()
+    {
+        if(playerSoul == null)
+        {
+            slider.maxValue = playerHealth.currentHealth;
+            //slider.value = playerHealth.currentHealth;
+            fill.color = gradient.Evaluate(1f);
+        }
+
+        else if (playerHealth == null)
+        {
+            //slider.maxValue = playerSoul.currentSouls;
+            //slider.value = playerHealth.currentHealth;
+        }
+
+    }
+
+    private void Update()
+    {
+        if(playerSoul == null)
+        {
+            slider.value = playerHealth.currentHealth;
+            fill.color = gradient.Evaluate(slider.normalizedValue);
+        }
+
+        else if (playerHealth == null)
+        {
+            slider.value = PlayerPrefs.GetFloat("soulCount");
+        }
+    }
+}
