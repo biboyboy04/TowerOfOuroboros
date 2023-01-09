d5526f497c98ebce9d556bf9293f4537fdf3d439
biboyboy04 <biboyboy04@gmail.com>
2023-01-09 08:53:43 +0800

    Refactor TeleportToPlayer.cs
    - Make teleport function a bool function so that it can choose to run the code or not instead of using multiple conditionals and calling the teleport function.
    
    - Wrap individual code blocks into functions
    
    - Improve variable names

--- Assets/Scripts/Enemy/TeleportToPlayer.cs
+++ Assets/Scripts/Enemy/TeleportToPlayer.cs
@@ -4,136 +4,87 @@ using UnityEngine;
 
 public class TeleportToPlayer : MonoBehaviour
 {
-    // The animation to play
     private Animator anim;
-    // The interval at which to play the animation, in seconds
-    public float interval;
-    // A timer to track when to play the animation
-    private float timer;
-
-    public float teleportDistance = 100f; // The distance at which the enemy will teleport
-    public GameObject player; // The player 
+    private Health enemyHealth;
+    private ChargeAttack chargeAttack;
+    
+    public float teleportCooldown;
+    public float teleportDistance = 100f;
     public Health playerHealth;
-    private Health health;
-
-    public bool canActivateAbility;
-    public bool activatingAbility;
-    public ChargeAttack chargeAttack;
-
-    private float offsetPosition;
-
+    public Transform playerTransform;
     public AudioSource bossTeleportSound;
 
-    bool canChangeValue = true;
+    private float timer;
+    private float distanceToPlayer;
+    private float teleportOffset = 1f;
+    private bool valueChanged = false;
+    private Vector2 playerFront;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();     
+        anim = GetComponent<Animator>(); 
+        enemyHealth = this.GetComponent<Health>();
+        chargeAttack = this.GetComponent<ChargeAttack>();
     }
 
-    void FixedUpdate() 
-    {
-        AbilityActivateIndicator();
-    }
-    
     // Update is called once per frame
     void Update()
     {   
-
-        if(IsHalfHealth())
-        {
-            canChangeValue = false;
-            interval/=2;
-        }
-        
         if(playerHealth.currentHealth > 0)
         {
-            if(chargeAttack!=null && chargeAttack.canTeleport)
-            {
-                chargeAttack.canTeleport= false;
-                canActivateAbility = true;
-                offsetPosition = 2;
-                TeleportBehind();
-            }
-            else
-            {
-                offsetPosition = 1f;
-            }
-            // Increment the timer by the amount of time that has passed since the last frame
             timer += Time.deltaTime;
 
-            // If the timer has reached the interval, it's time to do something
-            if (timer >= interval)
-            {   
-                canActivateAbility = true;
-                TeleportBehind();
-                timer = 0;
-            }
+            // Change into the teleport animation 1 second before the cooldown 
+            // as an indicator that the enemy will teleport
+            anim.SetBool("teleport", (timer >= teleportCooldown-1));
             
-            else
+            MakeBossStrongWhenHealthIsLow();
+            Teleport(timer >= teleportCooldown);
+
+            if(chargeAttack != null)
             {
-                canActivateAbility = false;
+                Teleport(chargeAttack.canTeleport);
+                chargeAttack.canTeleport = false;
             }
         }
     }
 
-    void AbilityActivateIndicator()
+
+    private void Teleport(bool canTeleport)
     {
-        // If the timer is 1 second before the interval, change the color of the boss
-        if(timer >= interval-1)
-        {   
-            activatingAbility = true;
-            anim.SetTrigger("teleport");
-            this.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 1);
-        }
-        else
+        if(canTeleport)
         {
-            activatingAbility = false;
-            this.GetComponent<SpriteRenderer>().color = Color.white;
+            bossTeleportSound.Play();
+            distanceToPlayer = Vector2.Distance(transform.position, playerTransform.transform.position);
+
+            if (distanceToPlayer < teleportDistance)
+            {
+                // If enemy is at the right side of the player
+                if(this.transform.position.x > playerTransform.transform.position.x)
+                {
+                    playerFront = playerTransform.transform.position - playerTransform.transform.right * teleportOffset;
+                } 
+                else
+                { 
+                    playerFront = playerTransform.transform.position - playerTransform.transform.right * -teleportOffset;
+                }   
+
+                // Set the enemy's position to the calculated position at the front of the player 
+                transform.position = playerFront; 
+            }
+            // Reset the timer
+            timer = 0;
+        }
+    }
+
+
+    private void MakeBossStrongWhenHealthIsLow()
+    {
+        if(enemyHealth.currentHealth <= (enemyHealth.startingHealth / 2) && !valueChanged)
+        {
+            teleportCooldown/=2;
+            valueChanged = true;
         } 
-    }
-
-     void TeleportBehind()
-    {
-        bossTeleportSound.Play();
-        // Calculate the distance between the enemy and the player
-        float distance = Vector2.Distance(transform.position, player.transform.position);
-
-        // If the distance is less than the teleport distance, teleport the enemy
-        if (distance < teleportDistance)
-        {
-            if(this.transform.position.x > player.transform.position.x)
-            {
-                //Get position to teleport at the back left side of the player
-                Vector2 behindPlayer = player.transform.position - player.transform.right * offsetPosition;
-
-                // Set the enemy's position to the calculated position behind the player
-                transform.position = behindPlayer;
-            } 
-
-            // If enemy is at the left side of the player
-            else
-            { 
-                //Get position to teleport at the back right side of the player
-                Vector2 behindPlayer = player.transform.position - player.transform.right * -offsetPosition;
-
-                // Set the enemy's position to the calculated position behind the player
-                transform.position = behindPlayer;
-            }     
-            
-        }
-    }
-
-    private bool IsHalfHealth()
-    {   
-
-        health = this.GetComponent<Health>();
-        if(canChangeValue)
-        {
-            return health.currentHealth <= (health.startingHealth / 2);
-        }
-        else
-            return false;
     }
 }
