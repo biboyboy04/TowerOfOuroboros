07706c5af58bdc845f6833152e79247dd97fee0e
biboyboy04 <biboyboy04@gmail.com>
2023-02-15 20:22:29 +0800

    Update floor6 and delete old game versions
    Add reference to border health bar

--- Assets/Scripts/Enemy/SpawnEnemies.cs
+++ Assets/Scripts/Enemy/SpawnEnemies.cs
@@ -25,11 +25,21 @@ public class SpawnEnemies : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+  
+    }
+
+    void Update() 
+    {
+       if(playerHealth.currentHealth > 0)
+        {
+            // Increment the timer by the amount of time that has passed since the last frame
+            timer += Time.deltaTime;
+        }
         MakeBossStrongWhenHealthIsLow();
         AbilityActivateIndicator();
-        EnemySpawn();  
+        EnemySpawn();
     }
 
 
@@ -49,28 +59,22 @@ public class SpawnEnemies : MonoBehaviour
 
     private void EnemySpawn()
     {
-        if(playerHealth.currentHealth > 0)
+        if (timer >= spawnInterval)
         {
-            // Increment the timer by the amount of time that has passed since the last frame
-            timer += Time.deltaTime;
-
-            if (timer >= spawnInterval)
+            spawnSound.Play();
+            for (int i = 0; i < numEnemiesToSpawn; i++)
             {
-                spawnSound.Play();
-                for (int i = 0; i < numEnemiesToSpawn; i++)
-                {
-                    // Calculate a random position within the spawn radius
-                    Vector3 spawnPos = transform.position + Random.insideUnitSphere * spawnRadius;
-                    // Instantiate the enemy prefab at the spawn position
-                    Instantiate(enemyToSummon, spawnPos, Quaternion.identity);
-                }
-                timer = 0;
+                // Calculate a random position within the spawn radius
+                Vector3 spawnPos = transform.position + Random.insideUnitSphere * spawnRadius;
+                // Instantiate the enemy prefab at the spawn position
+                Instantiate(enemyToSummon, spawnPos, Quaternion.identity);
             }
+            timer = 0;
         }
     }
     
 
-     private void MakeBossStrongWhenHealthIsLow()
+    private void MakeBossStrongWhenHealthIsLow()
     {
         if(enemyHealth.currentHealth <= (enemyHealth.startingHealth / 2) && canPowerUp)
         {
